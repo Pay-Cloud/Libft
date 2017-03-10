460451e9d88cda6d111a27d431e9434ab530f68d
Mehdi MEZIYANE <mmeziyan@e1r13p14.42.fr>
2017-03-10 18:05:47 +0100

    imcomplete

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -12,22 +12,66 @@
 
 #include "libft.h"
 
+static	char	*ft_strl(char const *s, char c)
+{
+	int i;
+	char *str;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	if (!(str = ft_strnew(i) + 1))
+		return (NULL);
+		ft_putchar('\n');
+	return (str);
+}
+
 char	**ft_strsplit(char const *s, char c)
 {
-	char	**str;
-	int		i;
-	int		j;
-	int		n;
 
+	int j;
+	int i;
+	char **str;
+	
+
+	str = NULL;
+	j = 0;
 	i = -1;
-	n = 0;
-	j = -1;
-	while (s)
+	while (*s)
 	{
-		while (s[j] && s[++j] != c)
-			str[n][++i] = s[j];
-		while (s[j] && s[j] == c)
-			j++;
+		while (*s == c)
+			s++;
+		if (*s)
+			str[j] = ft_strl(s, c);
+		while (*s && *s != c)
+		{
+
+			str[j][++i] = *s;
+			s++;
+		}
+		j++;
+	}
+	if (str)
+	{
+		str[i] = "\0";
+		return (str);
 	}
 	return (NULL);
+}
+
+int main(int ac, char **av)
+{
+	int i;
+	char **s;
+
+	i = -1;
+	s = malloc(40);
+	s =	ft_strsplit(av[1], av[2][0]);
+		return 0;
+	ft_putchar(s[0][0]);
+
+
+	while (s[++i])
+		ft_putstr(s[i]);
+
 }
